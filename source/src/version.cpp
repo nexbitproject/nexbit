0a5a091f52d95c743c6306d69db25af0177b30b5
BitBD <tanvirtex@gmail.com>
2019-12-05 10:10:32 +0600

    commit version

--- source/src/version.cpp
+++ source/src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Nexbit");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ""
+#    define GIT_COMMIT_ID "b03746e30e598ec0246511991ea1481867b06243"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
