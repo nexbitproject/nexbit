c94886b9c642a15ad2604f15c8660acc2b1e54f7
Tanvir Rahman <bitbd83@users.noreply.github.com>
2020-07-16 20:16:05 +0600

    no need genesis as checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,10 +27,6 @@ namespace Checkpoints
     //
    static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-       
-        ( 0,      uint256("0x000001a301f8cbe7e8a36b2e93b6224626afc263a293712a1281112cc34ec6c5") ) // genesis
-        ( 650,    uint256("0x95f9ee25d72333a11555da44d03e6991b80e8bc7d964c8bec4afb53316d1e556") )
-        ( 2799,   uint256("0xc6018be1d10900ca11cf9d4fe647f0639f101fbb0bdbb816f2bb202d52536187") )
         ( 12400,  uint256("0xe303ba9daf7374112a2078ae8f8ab01a4b53e3254ccb54b2e3899e6c3b1902eb") )
         ( 24800,  uint256("0x8d2634f408f8fb1c2f7a7766f759b6c07305950f8f5eb6d26ae83ea4af9ec9c6") )
         ( 29209,  uint256("0xaf4de031b03ce279ca9e405593186503e60b114725c37f6a087b62df78434fb1") )  //correct block height
